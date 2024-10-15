54bdbab7c5c17289cf926f7db5bad3e610d760ae
gabriel <gabaamazing21@gmail.com>
2024-10-15 01:54:05 +0100

    pushing syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Learning syntax """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """ the functions return and wait for random numbers"""
+    ran = random.uniform(0, max_delay)
+    await asyncio.sleep(ran)
+    return ran

c7c851af2e7857f6659497e565869132bbe6470f
gabriel <gabaamazing21@gmail.com>
2025-01-15 03:51:19 +0100

    async function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([values1, value2]) => {
     console.log(values1.body, value2.firstName, value2.lastName);
-  }).catch(() => console.log('Signup system offline'));
+  }).catch(() => console.Error('Signup system offline'));
 }

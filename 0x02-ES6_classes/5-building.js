00f7ddcaf3f54478ea2d6a154f04b5ad23fcdd74
gabriel <gabaamazing21@gmail.com>
2025-01-17 00:36:38 +0100

    implementing abstact class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiae an abstract class directly');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be number');
+    }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}

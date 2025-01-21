257ce1012cc98b9e783af36bc04f73572880a44b
gabriel <gabaamazing21@gmail.com>
2025-01-21 04:23:28 +0100

    iterate map and update value

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,4 @@
 export default function groceriesList() {
-  const array = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]
+  const array = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   return new Map(array);
 }

34fa40b5f6623fceeb7c115397e97e1f382807af
gabriel <gabaamazing21@gmail.com>
2025-01-23 10:04:47 +0100

    read file async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -11,57 +11,47 @@ function countStudents(filename) {
       throw new Error('Cannot load the database');
     }
     // read the file content
-    fs.readFile(filename, 'utf-8', (err, data) => {
-      if (err) {
-        console.log('Error reading the file', err);
-        return;
-      }
+    const data = fs.readFileSync(filename, 'utf-8');
 
-      const lines = data.trim().split('\n');
-      if (lines < 2) {
-        console.log('No student data found.');
-        return;
-      }
-      console.log(`Number of students: ${lines - 1}`);
+    const lines = data.trim().split('\n');
 
-      // extract the header line (first line)
-      const headers = lines[0].split(',');
-      // verify last column is 'field'
-      const lastColumnIndex = headers.length - 1;
-      const fieldName = headers[lastColumnIndex].trim();
-      console.log('counting student based on field name');
+    if (lines < 2) {
+      console.log('No student data found.');
+      return;
+    }
+    // extract the header line (first line)
+    // const headers = lines[0].split(',');
+    const fieldCounts = {};
 
-      // dictionary to count student in each fields
-      const fieldCount = {};
-      const firstNameIndex = 0;
-
-      for (let i = 1; i < lines.length; i += 1) {
-        const columns = lines[i].split(',');
-        const field = columns[lastColumnIndex].trim();
-        // extracting the first name
-        const firstName = columns[firstNameIndex].trim();
-
+    for (let i = 1; i < lines.length; i += 1) {
+      const fields = lines[i].split(',');
+      if (!(fields.length === 0 || fields[0].trim() === '')) {
+        // skip empty lines or invalid data
+        const field = fields[3];
+        const firstName = fields[0];
         // initialize the field entry if it doesn't exist
-        if (!fieldCount[field]) {
-          fieldCount[field] = {
-            count: 0,
-            names: [],
-          };
+        if (fieldCounts[field]) {
+          fieldCounts[field].count += 1;
+          fieldCounts[field].students.push(firstName);
+        } else {
+          fieldCounts[field] = { count: 1, students: [firstName] };
         }
-
-        // Increment the count for each field
-        fieldCount[field].count += 1;
-        fieldCount[field].names.push(firstName);
       }
+    }
 
-      // display the result
-      for (const [field, details] of Object.entries(fieldCount)) {
-        console.log(`Number of students in ${field}: ${details.count}.`);
-        console.log(`List: ${details.names.join(', ')}`);
+    const totalStudents = lines.length - 1;
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    // display the result
+    console.log(fieldCounts);
+    for (const field in fieldCounts) {
+      if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+        console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].students.join(', ')}`);
       }
-    });
-  } catch (err) {
-    console.error(err);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 }
 

dd87e4e922feeee4f59816a7a253084253d745b2
gabriel <gabaamazing21@gmail.com>
2025-01-24 14:10:00 +0100

    we stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,10 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (chunk) => {
+  const name = chunk.trim();
+  console.log(`Your name is: ${name}`);
+});
 
-rl.question('', (name) => {
-  if (name) console.log(`Your name is: ${name}`);
-});
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
-process.on('SIGINT', () => {
-  console.log('\nThis important software is now closing');
-  process.exit(0);
-});
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit(0);
 });

d725e2f292f2c139fc81bc5838af97285d9a98a5
gabriel <gabaamazing21@gmail.com>
2025-01-24 14:27:17 +0100

    we stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (chunk) => {
   const name = chunk.trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {

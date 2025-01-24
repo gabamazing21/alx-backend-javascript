7c9378a6985ec709809107cea76a361ee65dbf44
gabriel <gabaamazing21@gmail.com>
2025-01-24 14:15:29 +0100

    we stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to ALX, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (chunk) => {
   const name = chunk.trim();
-  console.log(`Your name is: ${name}\r`);
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {

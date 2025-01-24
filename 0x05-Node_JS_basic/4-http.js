df60b1d9d2fd06e63b8eedb665e4dd297397ea22
gabriel <gabaamazing21@gmail.com>
2025-01-24 15:21:13 +0100

    simple server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
 const port = 1245;
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.statusCode = 200;
   res.end('Hello ALX!');
 });

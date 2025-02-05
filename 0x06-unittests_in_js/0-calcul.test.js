15d7a3bb7bd01f6e43e65a3fcda96f0df7a83a9d
gabriel <gabaamazing21@gmail.com>
2025-02-05 01:28:46 +0100

    testing in node

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const calculateNumber = require('./0-calcul')
+const { describe } = require('node:test')
+
+describe('calculateNumber', ()=> {
+    it('should return the sum of rounded numbers', ()=>{
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 4.2), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should handle negative numbers', ()=>{
+        assert.strictEqual(calculateNumber(-1.4, -3.7), -5);
+        assert.strictEqual(calculateNumber(-1.2, -3.7), -5)
+    });
+
+    it('should handle zero correctly', ()=>{
+        assert.strictEqual(calculateNumber(0, 0), 0);
+        assert.strictEqual(calculateNumber(0, 3.7), 4);
+        assert.strictEqual(calculateNumber(-0.2, -0.7), -1);
+    });
+});
